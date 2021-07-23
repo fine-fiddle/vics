c5754f672ebee88a8dc308bd29bf4ba6aad14da9
jerome <jerome@whatever.com>
2021-07-22 19:52:31 -0500

    i cargo culted this from the youtube

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="vics",
+    version="0.0.0.1",
+    description="basis of visual image categorization tool"
+)
